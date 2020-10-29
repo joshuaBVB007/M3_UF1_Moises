143c00b8020f00b6ad66f99547ee253eaaad1e52
jonathan calderon <cf19jonathan.ascencio@iesjoandaustria.org>
2020-10-29 12:41:06 +0100

    preso hecho

---
+++ 01_14_preso/Preso.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Preso{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Scanner e=new Scanner(System.in);
+		System.out.println("Com us dieu?");
+		String nombre=Entrada.readLine();
+		
+		//Scanner et=new Scanner(System.in);
+		System.out.println("Quants anys teniu?");
+		int edad=Integer.parseInt(Entrada.readLine());
+		
+		
+		if(edad>=18){
+			System.out.println("Vos ja podeu anar a la presó!");
+			System.out.println("Aneu amb compte "+nombre);
+		}else{
+			System.out.println("Aneu amb compte "+nombre);
+		}
+		
+		
+
+	}
+
+}
