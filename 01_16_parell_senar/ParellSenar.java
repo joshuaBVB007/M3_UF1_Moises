705739ccb86632e2724587799312e8ea89f49bba
jonathan calderon <cf19jonathan.ascencio@iesjoandaustria.org>
2020-10-29 13:52:33 +0100

    pares e impares hecho

---
+++ 01_16_parell_senar/ParellSenar.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ParellSenar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numero=Integer.parseInt(args[0]);
+		
+		int residuo=numero%2;
+		
+		if(residuo==1){
+			System.out.println("El número "+numero+" és senar");
+		}else{
+			System.out.println("El número "+numero+" és parell");
+		}	
+	}
+}
