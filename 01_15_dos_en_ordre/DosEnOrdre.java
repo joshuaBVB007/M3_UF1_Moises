c7484a0a94707adf12a8f8ef2282917b957680de
jonathan calderon <cf19jonathan.ascencio@iesjoandaustria.org>
2020-10-29 13:30:00 +0100

    dos en orden hecho

---
+++ 01_15_dos_en_ordre/DosEnOrdre.java
@@ -0,0 +1,28 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DosEnOrdre {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Primer?");
+		int primero=Integer.parseInt(bf.readLine());
+		
+		System.out.println("Segon?");
+		int segundo=Integer.parseInt(bf.readLine());
+		
+		if(primero>segundo){
+			System.out.println(segundo+" i "+primero);
+		}else if(primero==segundo){
+			System.out.println(primero+" i "+segundo);
+		}else if(segundo>primero){
+			System.out.println(primero+" i "+segundo);
+		}
+		
+		
+	}
+
+}
