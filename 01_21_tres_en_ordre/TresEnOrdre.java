8814cfcb18f0ac9792da1876fe81210f9b0b6a76
jonathan calderon <cf19jonathan.ascencio@iesjoandaustria.org>
2020-10-31 15:33:16 +0100

    tres en orden  con capcalera hecho

--- 01_21_tres_en_ordre/TresEnOrdre.java
+++ 01_21_tres_en_ordre/TresEnOrdre.java
@@ -1,44 +1,38 @@
 
 import java.util.Scanner;
+
 public class TresEnOrdre {
+	
+	/*programa que ordena 3 numeros de menor a mayor*/
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	Scanner e=new Scanner(System.in);
-	System.out.println("Primer?");
-	int primero1=e.nextInt();
-
-	Scanner et=new Scanner(System.in);
-	System.out.println("Segon?");
-	int segundo2=et.nextInt();
-
-	Scanner ets=new Scanner(System.in);
-	System.out.println("Tercer?");
-	int tercero=ets.nextInt();
-		
-       if(primero1 >= segundo2){
-    	   /*si primero no es mayor que el segundo damos
-    	   por hecho que primero es inferior a segundo
-    	   que nos queda? validar el segundo y tercero*/
-			int intercambio = primero1; 
-			primero1 = segundo2; 
-			segundo2 = intercambio; 			
-		}
-	else if(primero1<=segundo2){
-		int intercambio=primero1;
-		segundo2=intercambio;
-		primero1=segundo2;
-	}else if(segundo2 >= tercero){
-			/*
-			 * si hemos llegado a este punto es porque el primero no superó 
-			 * al segundo entoces si segundo no es mayor que tres 
-			 * los numeros estan ordenados
-			 */
-			int intercambio = segundo2;
-			segundo2 = tercero;
-			tercero = intercambio;
-		}
-		System.out.print(primero1 + "," +" "+ segundo2 + " i " + tercero);
+		int mayor,medio,menor;
+		System.out.println("Primer?");
+		int primero=Integer.parseInt(Entrada.readLine());
+		System.out.println("Segon?");
+		int segundo=Integer.parseInt(Entrada.readLine());
+		System.out.println("Tercer?");
+		int tercero=Integer.parseInt(Entrada.readLine());
+	   if(primero > segundo && primero > tercero){
+		   mayor=primero;
+	   }else if(segundo>primero && segundo > tercero){
+		   mayor=segundo;
+	   }else{
+		   mayor=tercero;
+	   }
+			
+	   
+	   if(primero<segundo && primero<tercero){
+		   menor=primero;
+	   }else if(segundo<primero && segundo<tercero){
+		   menor=segundo;
+	   }else{
+		   menor=tercero;
+	   }
+		   
+	   medio=(primero+segundo+tercero)-(mayor+menor);
+		System.out.print(menor +","+" " + medio + " i " + mayor);
 		
 		
 	}
