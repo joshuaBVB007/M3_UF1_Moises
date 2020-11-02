5a2a3682890f0e79741ccf6c8f5aff59104d57d6
jonathan calderon <cf19jonathan.ascencio@iesjoandaustria.org>
2020-11-02 17:31:29 +0100

    ejercicio 1

---
+++ 01_37_deu_primers/DeuPrimers.java
@@ -0,0 +1,10 @@
+/*este programa hace un recorrido del numero 1....hasta el numero 10*/
+
+public class DeuPrimers{
+	public static void main(String[] args){
+	for(int i=1;i<=10;i++){
+		System.out.println(i);
+	}
+
+  }
+}
