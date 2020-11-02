9dc07d2dbfa6931c855d71dc84810c2d19e655d7
jonathan calderon <cf19jonathan.ascencio@iesjoandaustria.org>
2020-11-02 13:06:04 +0100

    hecho

---
+++ 01_25_nombre_de_fills/Fills.java
@@ -0,0 +1,24 @@
+/*que hace el programa */
+
+public class Fills{
+	public static void main(String[] args){
+
+	System.out.println("Quants fills tens?");
+	int hijos=Integer.parseInt(Entrada.readLine());
+
+	if(hijos<0){
+	  System.out.println("No pots tenir menys de 0 fills!");
+	}else if(hijos==0){
+	  System.out.println("Tot el que t'has estalviat en bolquers!");
+	}else if(hijos==1){
+	  System.out.println("Compte de no mimar-lo massa!");
+	}else if(hijos>1 && hijos<5){
+		System.out.println("No t'avorreixes a casa, eh?");
+	}else if(hijos>4){
+		System.out.println("Tu sí fas país!");
+	}
+
+
+
+	}
+}
