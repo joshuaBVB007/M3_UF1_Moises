f96ec4657d07ff35fb267fbd98ffb620880eef1f
jonathan calderon <cf19jonathan.ascencio@iesjoandaustria.org>
2020-11-02 17:07:42 +0100

    buen ejercicio

--- 01_35_endevina/Endevina.java
+++ 01_35_endevina/Endevina.java
@@ -1,3 +1,7 @@
+/*programa que nos permite ingresar infinitas veces valores hasta que se introduzca un 42 que es el numero para adivinar.
+si el numero introducido es inferior a cero dice fora de rang y si es mayor que 100 tambien.
+hasta que se haya ingresado un 42 se termina el programa nos muestra el texto Encertat*/
+
 public class Endevina{
 	public static void main(String[] args){
 	while(true){
