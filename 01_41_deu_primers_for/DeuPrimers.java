578d63fce91541035d3818ff58cbef75c634f502
jonathan calderon <cf19jonathan.ascencio@iesjoandaustria.org>
2020-11-07 18:22:01 +0100

    hecho

---
+++ 01_41_deu_primers_for/DeuPrimers.java
@@ -0,0 +1,11 @@
+/*el funcionamiento de este programa es el siguiente:hara un recorrido 
+de el numero 1 hasta el 10 con el bucle determinado for*/
+
+public class DeuPrimers{
+	public static void main(String[] args){
+
+	for(int i=1;i<=10;i++){
+		System.out.println(i);
+	}
+   }
+}
