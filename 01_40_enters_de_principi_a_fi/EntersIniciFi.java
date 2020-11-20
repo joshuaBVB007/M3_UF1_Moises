3998a9db940558842ac869ecd35753464d4bfde1
jonathan calderon <cf19jonathan.ascencio@iesjoandaustria.org>
2020-11-20 17:32:47 +0100

    finalizado

--- 01_40_enters_de_principi_a_fi/EntersIniciFi.java
+++ 01_40_enters_de_principi_a_fi/EntersIniciFi.java
@@ -1,3 +1,22 @@
+/*el funcionamiento de este programa es el siguiente:
+el usuario introduce 3 valores 
+el primero es desde donde empieza el recorrido
+el segundo valor donde finaliza 
+el tercer valor de cuanto en cuanto vamos saltando valores
+
+ejemplo: 
+valor inicial?
+9
+valor final?
+16
+Salt?
+3
+salida=9,12,15
+*/
+
+
+
+
 public class EntersIniciFi{
 	public static void main(String[] args){
 	System.out.println("Valor inicial?");
