256ba8121ca5c4cea4242569a4c8354b80cd7342
jonathan calderon <cf19jonathan.ascencio@iesjoandaustria.org>
2020-11-22 15:47:54 +0100

    finalizado

---
+++ 01_59_inicial_vocal_minuscula/LloroVocalMinuscules.java
@@ -0,0 +1,23 @@
+public class LloroVocalMinuscules{
+	public static void main(String[] args){
+	boolean c=false;
+	while(true){
+		System.out.println("El lloro pregunta paraula que comenci amb vocal en minúscules");
+		String paraula=Entrada.readLine();
+
+	if(paraula.startsWith("a") || paraula.startsWith("e") || paraula.startsWith("i") || paraula.startsWith("o") || paraula.startsWith("u")){
+
+
+	System.out.println("El lloro diu: "+paraula);
+
+	}else if(paraula.equals(" ") || paraula.isEmpty()){
+		System.out.println("Adéu");
+		break;
+	}else{
+
+	}
+
+	}//FIN WHILE
+
+  }
+}
