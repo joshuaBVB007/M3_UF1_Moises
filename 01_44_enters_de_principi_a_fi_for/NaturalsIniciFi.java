bbbb6f816c6c071b3aac7ca4f9620beb080440c1
jonathan calderon <cf19jonathan.ascencio@iesjoandaustria.org>
2020-12-09 10:42:13 +0100

    hecho

--- 01_44_enters_de_principi_a_fi_for/NaturalsIniciFi.java
+++ 01_44_enters_de_principi_a_fi_for/NaturalsIniciFi.java
@@ -1,3 +1,17 @@
+/*el funcionamiento de este programa es recibir 3 numeros por teclado 
+introducidos por el usuario: el primer valor es el valor desde donde se inicia
+el recorrido,el segundo donde termina el recorrido y el tercero de cuanto en
+cuanto iremos aumentando
+ejemplo:
+inicio=9
+fin=16
+salto=3
+
+salida=9,12,15
+
+*/
+
+
 public class NaturalsIniciFi{
 	public static void main(String[] args){
 
