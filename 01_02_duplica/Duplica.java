49a9d3411b467fabe3e9d6c8c115349e438fde37
jonathan calderon <cf19jonathan.ascencio@iesjoandaustria.org>
2020-12-13 01:15:39 +0100

    XD

---
+++ 01_02_duplica/Duplica.java
@@ -0,0 +1,11 @@
+public class Duplica{
+	public static void main(String[] args){
+        
+	int numero=Integer.parseInt(args[0]);
+
+	int num2=numero;
+
+	System.out.println("El doble de "+numero+" és "+(numero+num2));
+
+ }
+}
