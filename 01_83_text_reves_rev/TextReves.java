bf72c94f545711a13bd86c0cbea45126240a6596
jonathan calderon <cf19jonathan.ascencio@iesjoandaustria.org>
2020-12-13 13:56:42 +0100

    hecho

---
+++ 01_83_text_reves_rev/TextReves.java
@@ -0,0 +1,51 @@
+public class TextReves {
+	public static void main(String[] args) {
+		System.out.println("Text?");
+		String e=Entrada.readLine();
+		String filtro="";
+		int var=0;
+		char[] lista=e.toCharArray();
+		char[] copia=new char[lista.length];
+		
+		
+		//copiamos el modelo de cadena
+		for (int i = 0; i < lista.length; i++) {
+			//System.out.print(lista[i]);
+			if(Character.isLetter(lista[i])) {
+				copia[i]='i';
+			}else if(Character.isDigit(lista[i])){
+				copia[i]='i';
+			}else if (lista[i]==' ') {
+				copia[i]=' ';
+			}else if(!(lista[i]==' ') || !Character.isLetter(lista[i]) ) {
+				copia[i]=lista[i];
+			}
+		}
+		
+		//filtramos la cadena para coger solo las letras
+		for (int i = 0; i < lista.length; i++) {
+			if(Character.isLetter(lista[i])) {
+				filtro+=lista[i];
+			}else if(Character.isDigit(lista[i])){
+				filtro+=lista[i];
+			}
+		}
+		
+		char [] comparador=filtro.toCharArray();
+		var=comparador.length-1;
+		
+		for (int i = 0; i < copia.length; i++) {
+			if(copia[i]=='i') {
+				copia[i]=comparador[var];
+				var--;
+			}
+			System.out.print(copia[i]);
+		}
+		
+		//ema nwenem -a <ndl-omhc> <nar-btig>
+		
+		
+
+	}
+
+}
