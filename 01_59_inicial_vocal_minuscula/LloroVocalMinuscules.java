ce41e7a680abfbc8713fe575e93e22cac0a11214
jonathan calderon <cf19jonathan.ascencio@iesjoandaustria.org>
2020-12-14 12:34:22 +0100

    hecho

--- 01_59_inicial_vocal_minuscula/LloroVocalMinuscules.java
+++ 01_59_inicial_vocal_minuscula/LloroVocalMinuscules.java
@@ -1,3 +1,9 @@
+/*
+Este programa recibe constantemente palabras del usuario pero solo repite
+aquellas que empiecen con minuscules si introduces una palabra que inicia con
+majuscules no la imprime.
+Ahora? y si introduces una cadena vacia? el programa termina.
+*/
 public class LloroVocalMinuscules{
 	public static void main(String[] args){
 	boolean c=false;
