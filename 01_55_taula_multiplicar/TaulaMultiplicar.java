4044f89a89140ff6673cfa6c19fd8748a20c15b2
jonathan calderon <cf19jonathan.ascencio@iesjoandaustria.org>
2020-12-14 12:27:45 +0100

    Revision final hecha

--- 01_55_taula_multiplicar/TaulaMultiplicar.java
+++ 01_55_taula_multiplicar/TaulaMultiplicar.java
@@ -1,3 +1,18 @@
+/*
+El funcionamiento de este programa es el siguiente recibimos 4 parametros por parte del usuario y nos dibuja una lista personalizada 
+ej: 
+entrada 1 3 4 5
+salida 
+1*4=4
+1*5=5
+2*4=8
+2*5=10
+3*4=12
+3*5=15
+
+es decir 1 3 indican las tablas que quiero consultar luego 4 y 5 representan
+la operacion de esa tabla que yo quiero
+*/
 public class TaulaMultiplicar{
 	public static void main(String[] args){
 
