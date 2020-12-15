580a644df282b78e16690e02f07709d8f540c205
jonathan calderon <cf19jonathan.ascencio@iesjoandaustria.org>
2020-12-15 13:00:32 +0100

    Validado y con encabezado

--- 01_72_cerca_x/ConteDarrer.java
+++ 01_72_cerca_x/ConteDarrer.java
@@ -1,44 +1,54 @@
+/*Este programa tiene el funcionamiento siguiente:
+recibe del usuario una cadena de texto,si la ingresa por primera vez la recibe 
+y guarda su ultima letra,luego en la segunda cadena recibida verifica si contiene ese ultimo caracter guardado en el primer ingreso.
+
+ej:
+"Donam una A" 
+guarda esa A y revisa si la nueva cadena lo contiene.
+
+"Donam una B" como puedes ver en la nueva cadena introducida hay una a da igual si es mayuscula o minuscula cuenta.
+
+en cambio si introduces una cadena vacia se despide diciendo "Adéu"
+*/
 public class ConteDarrer{
    public static void main(String[] args){
 
 	System.out.println("Introdueix texts (enter sol per finalitzar)");
 	int inicial=0;
-	char[] lista=null;
 	char memoria=' ';
 	int validador=0;
 	while(true){
 		String cadena=Entrada.readLine();
+		char[] lista=cadena.toCharArray();
 		if(cadena.isEmpty()){
 			System.out.println("Adéu");
 			break;
 		}else if(inicial==0){
-			lista=cadena.toCharArray();
-			memoria=lista[lista.length-1];
+			//System.out.println(cadena.charAt(lista.length-1));
+			memoria=cadena.charAt(lista.length-1);
 			inicial++;
 			System.out.println("bé");
-			//System.out.println(memoria);
 		}else if(inicial>0){
+			validador=0;
 			for(int i=0;i<lista.length;i++){
-			lista=cadena.toCharArray();
-			String conversor=String.valueOf(lista[i]);
-			String conversor_memoria=String.valueOf(memoria);
-			if(conversor.equalsIgnoreCase(conversor_memoria)){
-				System.out.println("bé");
-				validador++;
-				break;
+				String e=String.valueOf(lista[i]);
+				String t=String.valueOf(memoria);
+				if(e.equalsIgnoreCase(t)){
+					memoria=cadena.charAt(lista.length-1);
+					//System.out.println(memoria+"-"+lista[i]);
+					validador++;
+					break;
+				}
 			}
-			}
-			if(validador==0){
-				System.out.println("Adéu");
-				break;
+			if(validador>0){
+			    System.out.println("bé");
 			}else{
-				memoria=lista[lista.length-1];
+			    System.out.println("Adéu");
+			    break;
 			}
-			
-
-			
 		}
 	}
+	
 
  }
 }
